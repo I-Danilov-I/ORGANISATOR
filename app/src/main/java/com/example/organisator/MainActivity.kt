fe632d750b48c3a4fa831e9a6c95fcf37bfccbb2
ARHITEKTOR <danilov@mail.de>
2023-11-11 23:52:26 +0100

    Update

--- app/src/main/java/com/example/organisator/MainActivity.kt
+++ app/src/main/java/com/example/organisator/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.organisator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val floatingAnimation = AnimationUtils.loadAnimation(this, R.anim.floating_text)
+        textView = findViewById(R.id.textView)
+        textView.startAnimation(floatingAnimation)
     }
 }
