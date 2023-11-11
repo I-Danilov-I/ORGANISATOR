aa2e9cea61c660c47211a48ebb418eed357ed6e1
ARHITEKTOR <danilov@mail.de>
2023-11-12 00:49:40 +0100

    Update

--- app/src/main/java/com/example/organisator/MainActivity.kt
+++ app/src/main/java/com/example/organisator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.organisator
 
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,8 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Animation
         val floatingAnimation = AnimationUtils.loadAnimation(this, R.anim.floating_text)
         textView = findViewById(R.id.textView)
         textView.startAnimation(floatingAnimation)
+
+        // Erstelle eine veränderliche Liste
+        val liste = mutableListOf("Äpfel", "Birnen", "Eier")
+        liste.add("Wasser")
+
+        // Erstelle einen ArrayAdapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, liste)
+
+        // Finde die ListView und setze den Adapter
+        val listView = findViewById<ListView>(R.id.ListView)
+        listView.adapter = adapter
     }
 }
