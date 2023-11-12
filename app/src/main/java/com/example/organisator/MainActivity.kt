d80b50f53f10e9a24629a800295d07e9118c3618
ARHITEKTOR <danilov@mail.de>
2023-11-12 16:25:19 +0100

    Update

--- app/src/main/java/com/example/organisator/MainActivity.kt
+++ app/src/main/java/com/example/organisator/MainActivity.kt
@@ -14,31 +14,44 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    // Deklaration der Liste und des Adapters auf Klassenebene, damit sie in mehreren Methoden verwendet werden können
+    private lateinit var liste: MutableList<String>
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Ersetzen Sie dies durch die ID Ihres ImageView-Elements
+        // Aufruf der Setup-Methoden
+        setupAnimation()
+        setupList()
+        setupButtons()
+    }
+
+    // Methode zum Einrichten der Animation
+    private fun setupAnimation() {
+        // Finden Sie das TextView-Element und laden Sie die Animation
         val textanimation = findViewById<TextView>(R.id.textView)
-        // Laden Sie die Animation
         val animation = AnimationUtils.loadAnimation(this, R.anim.floating_text)
 
         // Starten Sie die Animation
         textanimation.startAnimation(animation)
+    }
 
-        // Erstelle eine veränderliche Liste
-        val liste = mutableListOf("Äpfel", "Birnen", "Eier")
+    // Methode zum Einrichten der Liste
+    private fun setupList() {
+        // Erstellen Sie die Liste und den Adapter
+        liste = mutableListOf("Äpfel", "Birnen", "Eier")
+        adapter = MyAdapter(this, liste)
 
-        // Erstelle einen ArrayAdapter
-        val adapter = MyAdapter(this, liste)
-
-        // Finde die ListView und setze den Adapter
+        // Finden Sie die ListView und setzen Sie den Adapter
         val listView = findViewById<ListView>(R.id.ListView)
         listView.adapter = adapter
 
-        // Beim langem Drücken auf einen Eintrag wird er gelöscht
+        // Setzen Sie einen OnItemLongClickListener für die ListView
         listView.setOnItemLongClickListener { _, _, position, _ ->
             // Entfernen Sie den Eintrag an der angeklickten Position aus der Liste
             liste.removeAt(position)
@@ -46,57 +59,47 @@ class MainActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
             true
         }
+    }
 
-        // Set Button begeung
+    // Methode zum Einrichten der Buttons
+    private fun setupButtons() {
+        // Finden Sie die Buttons und setzen Sie die OnClickListener
         val settingsButton = findViewById<ImageButton>(R.id.set_button)
         val setbuttonanimation = AnimationUtils.loadAnimation(this, R.anim.set_button)
         settingsButton.startAnimation(setbuttonanimation)
 
-
-        // Sobal das Textfeld zur Eingabe angeklickt wird, bereits stehen TExt löschen
-        val textEingabe = findViewById<EditText>(R.id.TextEingabe)
-        textEingabe.setOnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                // Löschen Sie den Text, wenn das EditText-Element den Fokus erhält
-                (v as EditText).text.clear()
-            }
-        }
-
-
-        // Hinzufüge Button
         val addButton = findViewById<ImageButton>(R.id.addButton)
         addButton.setOnClickListener {
-            // Abrufen des Texts aus dem EditText-Feld
+            val textEingabe = findViewById<EditText>(R.id.TextEingabe)
             val eingabeText = textEingabe.text.toString()
-            // Überprüfen, ob der Text nicht leer ist
             if (eingabeText.isNotEmpty()) {
-                // Fügen Sie den eingegebenen Text zur Liste hinzu
                 liste.add(eingabeText)
-                // Löschen Sie den Text im EditText-Feld
                 textEingabe.text.clear()
-                // Benachrichtigen Sie den Adapter, dass sich die Daten geändert haben
                 adapter.notifyDataSetChanged()
             }
         }
 
+        val textEingabe = findViewById<EditText>(R.id.TextEingabe)
+        textEingabe.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                (v as EditText).text.clear()
+            }
+        }
     }
 
-
-
-    // Farbe für Die Listen einträge ändern für die Ausgabe
+    // Innere Klasse für den benutzerdefinierten ArrayAdapter
     inner class MyAdapter(context: Context, liste: List<String>) : ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, liste) {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             val view = super.getView(position, convertView, parent)
             val textView = view.findViewById<TextView>(android.R.id.text1)
 
-            // Ändern Sie hier die Farbe
+            // Ändern Sie die Farbe und die Schriftgröße des Texts
             textView.setTextColor(ContextCompat.getColor(context, R.color.colorTextListe))
-
-            // Ändern Sie hier die Schriftgröße
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)  // Setzen Sie hier die gewünschte Schriftgröße ein
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
 
             return view
         }
     }
 }
+
