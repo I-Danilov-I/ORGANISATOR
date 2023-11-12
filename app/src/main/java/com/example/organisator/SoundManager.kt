0e6c9058ddd7e5eaaf65262108b3e7fa0ad2f56f
ARHITEKTOR <danilov@mail.de>
2023-11-12 18:15:18 +0100

    Update

---
+++ app/src/main/java/com/example/organisator/SoundManager.kt
@@ -0,0 +1,11 @@
+package com.example.organisator
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class SoundManager(private val context: Context) {
+    fun playSound() {
+        val mediaPlayer = MediaPlayer.create(context, R.raw.sound_file)
+        mediaPlayer.start()
+    }
+}
