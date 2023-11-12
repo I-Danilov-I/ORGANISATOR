f54b0cc9b67dc6317221afe2ee65386ebe118928
ARHITEKTOR <danilov@mail.de>
2023-11-12 01:03:09 +0100

    Update

--- app/src/main/java/com/example/organisator/MainActivity.kt
+++ app/src/main/java/com/example/organisator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.organisator
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.view.animation.AnimationUtils
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
@@ -14,20 +18,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Animation
-        val floatingAnimation = AnimationUtils.loadAnimation(this, R.anim.floating_text)
-        textView = findViewById(R.id.textView)
-        textView.startAnimation(floatingAnimation)
-
         // Erstelle eine veränderliche Liste
         val liste = mutableListOf("Äpfel", "Birnen", "Eier")
         liste.add("Wasser")
 
         // Erstelle einen ArrayAdapter
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, liste)
+        val adapter = MyAdapter(this, liste)
 
         // Finde die ListView und setze den Adapter
         val listView = findViewById<ListView>(R.id.ListView)
         listView.adapter = adapter
+    }
+
+    inner class MyAdapter(context: Context, private val liste: List<String>) : ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, liste) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent)
+            val textView = view.findViewById<TextView>(android.R.id.text1)
+
+            // Ändern Sie hier die Farbe
+            textView.setTextColor(Color.RED)  // Setzen Sie hier die gewünschte Farbe ein
+
+            // Ändern Sie hier die Schriftgröße
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)  // Setzen Sie hier die gewünschte Schriftgröße ein
+
+            return view
+        }
     }
 }
