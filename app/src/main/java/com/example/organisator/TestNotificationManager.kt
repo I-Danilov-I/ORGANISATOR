6613d1fd83b6c1280c9b1fbb948dd18c46b28362
ARHITEKTOR <danilov@mail.de>
2023-11-13 17:25:59 +0100

    Update

---
+++ app/src/main/java/com/example/organisator/TestNotificationManager.kt
@@ -0,0 +1,27 @@
+package com.example.organisator
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class TestNotificationManager(private val context: Context) {
+
+    fun sendTestNotification() {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("default", "Test Notification", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(context, "default")
+            .setContentTitle("Testbenachrichtigung")
+            .setContentText("Dies ist eine Testbenachrichtigung!")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+}
