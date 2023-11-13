9b98f900b4b5b25cca2fa28d5971f3c5118d8f5f
ARHITEKTOR <danilov@mail.de>
2023-11-13 19:35:07 +0100

    Update

--- app/src/main/java/com/example/organisator/AlarmReceiver.kt
+++ app/src/main/java/com/example/organisator/AlarmReceiver.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.core.app.NotificationCompat
 
@@ -18,10 +19,13 @@ class AlarmReceiver : BroadcastReceiver() {
             notificationManager.createNotificationChannel(channel)
         }
 
+
+        val largeIcon = BitmapFactory.decodeResource(context.resources, R.mipmap.app_icon_foreground)
         val notification = NotificationCompat.Builder(context, "default")
             .setContentTitle("Tägliche Erinnerung")
             .setContentText("Hier sind Ihre heutigen Einträge!")
-            .setSmallIcon(R.mipmap.app_icon_foreground)
+            .setSmallIcon(android.R.drawable.star_big_on)
+            .setLargeIcon(largeIcon)
             .build()
 
         notificationManager.notify(1, notification)
