5ff19e0e8453a241a2556c35e682eadc10062064
ARHITEKTOR <danilov@mail.de>
2023-11-14 17:30:56 +0100

    Update

--- app/src/main/java/com/example/organisator/NotificationManager.kt
+++ app/src/main/java/com/example/organisator/NotificationManager.kt
@@ -3,8 +3,11 @@ package com.example.organisator
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 
 class NotificationManager(private val context: Context) {
+
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("NotificationPrefs", Context.MODE_PRIVATE)
 
     private fun openNotificationSettings() {
         val intent = Intent().apply {
@@ -22,15 +25,18 @@ class NotificationManager(private val context: Context) {
     }
 
     fun showNotificationSettingsDialog() {
-        AlertDialog.Builder(context)
-            .setTitle("Benachrichtigung Aktivieren")
-            .setMessage("Wir Respektieren ihre Wahl! " +
-                    "Es wird jedoch Empfolen die Benachrichtugung für diese App zu Aktivieren." +
-                    "Benachrichtigungseinstellungen öffnen? ")
-            .setPositiveButton("Ja") { _, _ ->
-                openNotificationSettings()
-            }
-            .setNegativeButton("Nein", null)
-            .show()
+        if (!sharedPreferences.getBoolean("dialogShown", false)) {
+            AlertDialog.Builder(context)
+                .setTitle("Benachrichtigung Aktivieren")
+                .setMessage("Wir Respektieren ihre Wahl! " +
+                        "Es wird jedoch Empfolen die Benachrichtugung für diese App zu Aktivieren." +
+                        "Benachrichtigungseinstellungen öffnen? ")
+                .setPositiveButton("Ja") { _, _ ->
+                    openNotificationSettings()
+                    sharedPreferences.edit().putBoolean("dialogShown", true).apply()
+                }
+                .setNegativeButton("Nein", null)
+                .show()
+        }
     }
 }
