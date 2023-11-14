3d59d4d19a8b21102b25c238b89d1bbc2e69bed4
ARHITEKTOR <danilov@mail.de>
2023-11-14 20:09:04 +0100

    Update

--- app/src/main/java/com/example/organisator/ListDataManager.kt
+++ app/src/main/java/com/example/organisator/ListDataManager.kt
@@ -1,9 +1,52 @@
 package com.example.organisator
 
 import android.content.Context
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
+class MyAdapter(context: Context, liste: List<String>) : ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, liste) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = super.getView(position, convertView, parent)
+        val textView = view.findViewById<TextView>(android.R.id.text1)
+
+        // Ändern Sie die Farbe und die Schriftgröße des Texts
+        textView.setTextColor(ContextCompat.getColor(context, R.color.colorTextListe))
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+
+        return view
+    }
+}
+
+class ListSetup(private val activity: AppCompatActivity,
+                private val liste: MutableList<String>,
+                private val adapter: ArrayAdapter<String>,
+                private val soundManager: SoundManager) {
+    // Methode zum Einrichten der Liste
+    fun setupList() {
+        // Finden Sie die ListView und setzen Sie den Adapter
+        val listView = activity.findViewById<ListView>(R.id.ListView)
+        listView.adapter = adapter
+
+        // Setzen Sie einen OnItemLongClickListener für die ListView
+        listView.setOnItemLongClickListener { _, _, position, _ ->
+            soundManager.playSound(R.raw.sound_file_delete)
+            // Entfernen Sie den Eintrag an der angeklickten Position aus der Liste
+            liste.removeAt(position)
+            // Benachrichtigen Sie den Adapter, dass sich die Daten geändert haben
+            adapter.notifyDataSetChanged()
+            true
+        }
+    }
+}
 
 class ListDataManager(private val context: Context) {
     fun saveList(list: MutableList<String>) {
